b93c024168037c8f0f3743e1859e4a280f9fab0e
Yoseph Hasson <23007047+MDbrosev@users.noreply.github.com>
2018-05-05 04:18:45 -0400

    update 5
    5

---
+++ Assets/Scripts/cameraControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControls : MonoBehaviour
+{
+
+    public GameObject target;
+    private Vector3 targetposition;
+    public float cameraSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Target for camera, get the X and Y value of the target to follow and dont change the Z value since game is 2D
+        //Param: X, Y, Z of player
+        targetposition = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        //Move the camera to the target position
+        //Param: Vector3 a = current camera position, Vector3 b = the player location, float f = the amount of movement the camera can have in a single update frame
+        transform.position = Vector3.Lerp(transform.position, targetposition, cameraSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/nextArea.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//get to next map
+public class nextArea : MonoBehaviour
+{
+    public string level;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {   //If player collides with the trigger, load next level
+        if(collision.gameObject.name == "Player")
+        {
+            //This line is used to load the different scenes
+            Application.LoadLevel(level);
+        }
+    }
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
